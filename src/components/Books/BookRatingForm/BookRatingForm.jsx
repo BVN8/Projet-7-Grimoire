2080188a858f61cc5ace63649dac5217235ab21c
Benjamin Vincent-Neveu <b.vincent.neveu@gmail.com>
2024-07-05 10:10:31 +0200

    Premier commit

--- src/components/Books/BookRatingForm/BookRatingForm.jsx
+++ src/components/Books/BookRatingForm/BookRatingForm.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable no-console */
+/* eslint-disable no-alert */
 import * as PropTypes from 'prop-types';
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
